de5473a8efa5bb2818c109f8b0466061cf9c0065
Başak Özenalp Cesur <basak.ozenalp@gmail.com>
2024-11-30 14:59:13 +0300

    S14D4 çözüldü, string ID değiştirildi.

--- src/main/java/org/example/model/Store.java
+++ src/main/java/org/example/model/Store.java
@@ -4,9 +4,20 @@ public class Store {
     public static void main(String[] args) {
 
         //listProducts();
+
+        ProductForSale[] products = new ProductForSale[5];
+        products[0] = new Chocolate("sweet", 15, "chc dec", "white", true);
+        products[1] = new Bread("Bakery", 7,"bread dec", "health", "white");
+        products[2] = new Coke("Drinks", 10, "coke dec", true, 2.5);
+
+        listProducts(products);
     }
 
     public static void listProducts(ProductForSale[] products) {
-
+            for(ProductForSale product : products){
+                if(product != null){
+                    product.showDetails();
+                }
+            }
     }
 }
---
+++ src/main/java/org/example/rpg/Poisonable.java
@@ -0,0 +1,9 @@
+package org.example.rpg;
+
+public interface Poisonable {
+
+    public double poison();
+
+    public double poison(int level);
+
+}
